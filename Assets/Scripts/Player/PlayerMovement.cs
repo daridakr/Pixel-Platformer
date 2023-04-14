9c7306dc90bc111995deb3ce9d5e84ad01f13ced
Krivushkina Daria <daridakr@gmail.com>
2023-04-14 20:41:36 +0300

    Added physics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private Rigidbody2D _body;
     private Vector2 _direction;
-    private Vector3 _position;
+
+    private void Awake()
+    {
+        _body= GetComponent<Rigidbody2D>();
+    }
 
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        _position = transform.position;
-
-        if (_direction.magnitude > 0)
-        {
-            var delta = _direction * _speed * Time.deltaTime;
-            transform.position = _position + new Vector3(delta.x, delta.y, _position.z);
-        }
+        _body.velocity = new Vector2(_direction.x * _speed, _body.velocity.y);
     }
 }
