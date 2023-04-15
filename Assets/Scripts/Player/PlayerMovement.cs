c333995e0a9e235eae4b878a9fe26b42be67eb1b
Krivushkina Daria <daridakr@gmail.com>
2023-04-15 14:24:25 +0300

    Added a jumping through rb.Cast

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private ContactFilter2D _groundCheckFilter;
+    [SerializeField] private float _groundChekDistance = 0.1F;
 
-    private Rigidbody2D _body;
     private Vector2 _direction;
+    private Rigidbody2D _body;
 
     private void Awake()
     {
@@ -20,6 +23,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Move();
+
+        bool isJumping = _direction.y > 0;
+        if (isJumping && IsGrounded())
+        {
+            _body.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Move()
+    {
         _body.velocity = new Vector2(_direction.x * _speed, _body.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D[] results = new RaycastHit2D[1];
+        int collisionCount = _body.Cast(Vector2.down, _groundCheckFilter, results, _groundChekDistance);
+        return collisionCount != 0;
     }
 }
